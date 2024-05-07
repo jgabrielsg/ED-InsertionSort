34e3f48bb7b6236d12526fbaa8b6d10529f7c664
João Gabriel Machado <jogabriel433@gmail.com>
2024-05-07 15:21:13 -0300

    BubbleSort e OptimizedBubbleSort

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <chrono>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
+using namespace std;
+using namespace std::chrono;
+
+#include <cstdlib> // Para usar a função rand()
+#include <ctime> // Para usar a função time()
 
 typedef struct Node
 {
@@ -15,24 +17,135 @@ typedef struct Node
 Node* createNode(int);
 void insertEnd(Node**, int);
 void displayList(Node*);
-void swapValues(Node*, Node*);
 
+void swapPayload(Node*, Node*);
+void bubbleSort(Node*);
+void optimizedBubbleSort(Node*);
 
 int main()
 {
+    srand(time(0)); // Inicializa a semente do gerador de números aleatórios
+
     Node* head = nullptr;
+    Node* head2 = nullptr;
     
-    insertEnd(&head,0);
-    insertEnd(&head,1);
-    insertEnd(&head,3);
-    insertEnd(&head,7);
-    insertEnd(&head,10);
-    insertEnd(&head,13);
+    for(int i = 0; i < 100; i++)
+    {
+        int payload = rand() % 100; // Gera um número aleatório entre 0 e 99
+        insertEnd(&head, payload);
+        insertEnd(&head2, payload);
+    }
     
+    cout << "====================================" << endl;
+    cout << "UTILIZANDO bubbleSort" << endl;
+    cout << "====================================" << endl;
     displayList(head);
-    cout << "==============" << endl;
+    
+    
+    // Cálculo de tempo de duração da função:
+    auto timeStart = high_resolution_clock::now();
+    bubbleSort(head);
+    auto timeEnd = high_resolution_clock::now();
+    cout << "====================================" << endl;
+    displayList(head);
+    cout << "====================================" << endl;
+    
+    auto timeDuration = duration_cast<nanoseconds>(timeEnd - timeStart);
+    cout << "Tempo utilizado: " << timeDuration.count() << " nanosegundos" << endl;
+    
+    cout << "====================================" << endl;
+    cout << "UTILIZANDO optimizedBubbleSort" << endl;
+    cout << "====================================" << endl;
+    
+    displayList(head2);
+    cout << "====================================" << endl;
+    
+    // Cálculo de tempo de duração da função:
+    auto timeStart2 = high_resolution_clock::now();
+    optimizedBubbleSort(head2);
+    auto timeEnd2 = high_resolution_clock::now();
+    cout << "====================================" << endl;
+    displayList(head2);
+    cout << "====================================" << endl;
+    
+    auto timeDuration2 = duration_cast<nanoseconds>(timeEnd2 - timeStart2);
+    cout << "Tempo utilizado: " << timeDuration2.count() << " nanosegundos" << endl;
     
     return 0;
+}
+
+void swapPayload(Node* node1, Node* node2)
+{
+    int iTemp = node1->iPayload;
+    node1->iPayload = node2->iPayload;
+    node2->iPayload = iTemp;
+}
+
+void bubbleSort(Node* node)
+{
+    if (node == nullptr)
+    {
+        cout << "Lista vazia: Não é possível realizar bubbleSort" << endl;
+        return;
+    }
+    
+    if (node -> ptrPrev != nullptr)
+    {
+        cout << "Meio ou Fim da Lista: Não é possível realizar bubbleSort" << endl;
+        return;
+    }
+    
+    Node* outerLoop = node;
+    
+    while(outerLoop != nullptr && outerLoop->ptrNext != nullptr)
+    {
+        Node* current = node;
+        while(current != nullptr && current->ptrNext != nullptr)
+        {
+            if(current->iPayload > current->ptrNext->iPayload)
+            {
+                swapPayload(current, current->ptrNext);
+            }
+            current = current->ptrNext;
+        }
+        outerLoop = outerLoop->ptrNext;
+    }
+    
+    cout << "Finalizado" << endl;
+}
+
+
+void optimizedBubbleSort(Node* node)
+{
+    if (node == nullptr)
+    {
+        cout << "Lista vazia: Não é possível realizar bubbleSort" << endl;
+        return;
+    }
+    
+    if (node -> ptrPrev != nullptr)
+    {
+        cout << "Meio ou Fim da Lista: Não é possível realizar bubbleSort" << endl;
+        return;
+    }
+    
+    bool notOrdered;
+    do
+    {
+        notOrdered = false;
+        Node* current = node;
+        while(current->ptrNext != nullptr)
+        {
+            if(current->iPayload > current->ptrNext->iPayload)
+            {
+                swapPayload(current, current->ptrNext);
+                notOrdered = true;
+            }
+            current = current->ptrNext;
+        }
+    } while(notOrdered);
+    
+    cout << "Finalizado" << endl;
 }
 
 Node* createNode(int iPayload)
@@ -45,13 +158,12 @@ Node* createNode(int iPayload)
     return temp;
 }
 
-
 void displayList(Node* node)
 {
     if (node == nullptr)
     {
         cout << "Lista vazia: Não é possível realizar displayList" << endl;
-        return;
+        return; //Usa return pra sair da função, o resto do código não será executado
     }
     
     if  (node -> ptrPrev != nullptr)
@@ -76,25 +188,22 @@ void displayList(Node* node)
 
 void insertEnd(Node** head, int iPayload)
 {
-    Node* newNode = createNode(iPayload);
-    
-    if (*head == nullptr)
-    {
-        (*head) = newNode;
-        return;
-    }
-    
-    Node* temp = (*head);
-    
-    while(temp->ptrNext != nullptr) temp = temp->ptrNext;
-    
-    newNode->ptrPrev = temp;
-    temp->ptrNext = newNode; 
-}
-
-void swapValues(Node* node1, Node* node2) 
-{
-    int temp = node1->iPayload;
-    node1->iPayload = node2->iPayload;
-    node2->iPayload = temp;
+  Node* newNode = createNode(iPayload);
+  //newNode -> ptrNext = nullptr;
+  
+  if (*head == nullptr)
+  {
+      //newNode -> ptrNext = nullptr; Essa linha não é obrigatória pois já definimos anteriormente
+      (*head) = newNode;
+      return;
+      
+  }
+  
+  Node* temp = (*head);
+  
+  //Percorremos a lista até seu fim(ptrNext do ultimo nó é NULL)
+  while(temp->ptrNext != nullptr) temp = temp->ptrNext;
+  
+  newNode->ptrPrev = temp; //newNode aponta para o fim da lista
+  temp->ptrNext = newNode; //Antigo último elemento aponta para o novo nó
 }
